af47b49b12a0a6f91873f2e90b3e282d7ff6853e
Tom Moraiz <tom.moraiz@hetic.net>
2018-01-14 16:52:13 +0100

    [ADD] re-organized everything

---
+++ js/sceneManager.js
@@ -0,0 +1,69 @@
+function SceneManager(canvas) {
+
+  const clock = new THREE.Clock();
+
+  const screenDimensions = {
+    width: canvas.width,
+    height: canvas.height
+  }
+
+  const scene = buildScene();
+  const renderer = buildRender(screenDimensions);
+  const camera = buildCamera(screenDimensions);
+  const sceneSubjects = createSceneSubjects(scene);
+  const controls = new THREE.OrbitControls(camera);
+
+  controls.update();
+  camera.position.z = 5;
+
+  function buildScene() {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color("#000");
+
+    return scene;
+  }
+
+  function buildRender({width, height}) {
+    const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true, alpha: true});
+    const DPR = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
+    renderer.setPixelRatio(DPR);
+    renderer.setSize(width, height);
+
+    return renderer;
+  }
+
+  function buildCamera({width, height}) {
+    const aspectRatio = width / height;
+    const fieldOfView = 60;
+    const nearPlane = 0.1;
+    const farPlane = 1000;
+    const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+
+    return camera;
+  }
+
+  function createSceneSubjects(scene) {
+    const sceneSubjects = new SceneSubjects(scene)
+
+    return sceneSubjects;
+  }
+
+  this.update = function() {
+    renderer.render(scene, camera);
+    controls.update();
+    sceneSubjects.update();
+  }
+
+  this.onWindowResize = function() {
+    const { width, height } = canvas;
+
+    screenDimensions.width = width;
+    screenDimensions.height = height;
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+  }
+
+}
